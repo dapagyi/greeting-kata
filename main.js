ed90e47d8acf1c129dae49e3b73daf0539f84f04
Apagyi Dávid <apagyi.david@gmail.com>
2018-02-28 09:57:46 +0100

    frame

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 function greeting(msg,name){
-    console.log(msg +', ' + name + '!');
+    return (msg +', ' + name + '!');
 }
-greeting('Happy Eastern','Sasha');
+
+function logFrame(msg){
+    var margin = (80-msg.length)/2;
+    var line = '';
+    var marginline = '';
+    
+    for (var i=0; i<msg.length+2*margin; i++) line+='-';
+    for (var i=0; i<margin; i++) marginline+=' ';
+    
+    console.log();
+    console.log(line);
+    console.log(marginline+msg+marginline);
+    console.log(line);
+    console.log();
+}
+
+logFrame(greeting('Happy Eastern','Sasha'));
