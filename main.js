7aa39319502e4676372820c8e21c01995d5a0f52
Apagyi Dávid <apagyi.david@gmail.com>
2018-02-28 10:20:33 +0100

    #

--- main.js
+++ main.js
@@ -2,21 +2,31 @@ function greeting(msg,name='my friend'){
     return (msg +', ' + name + '!');
 }
 
-function logFrame(msg){
+function replaceAt(string, index, replace) {
+    return string.substring(0, index) + replace + string.substring(index + 1);
+}
+
+function logFrame(msg, framecharacter){
     var margin = (80-msg.length)/2;
     var line = '';
     var marginline = '';
-    
-    for (var i=0; i<msg.length+2*margin; i++) line+='*';
+    var borderline = '';
+
+    for (var i=0; i<msg.length+2*margin; i++) line+=framecharacter;
     for (var i=0; i<margin; i++) marginline+=' ';
-    
-    console.log();
+    for (var i=0; i<80; i++) borderline+=' ';
+    borderline=replaceAt(borderline,0,framecharacter);
+    borderline=replaceAt(borderline,79,framecharacter);
+
+    var msgline=replaceAt(marginline+msg+marginline,0,framecharacter).substring(0,79);
+    msgline=replaceAt(msgline,79,framecharacter);
+
     console.log(line);
-    console.log();
-    console.log(marginline+msg+marginline);
-    console.log();
+    console.log(borderline);
+    console.log(msgline);
+    console.log(borderline);
     console.log(line);
-    console.log();
+    console.log(line.length,msgline.length);
 }
 
-logFrame(greeting('Happy Eastern'));
+logFrame(greeting('Happy Eastern'),'#');
